35377ad91c034f94ec28dde5f5183f437d3824de
naftalimurgor <murgornaftali@gmail.com>
2022-08-20 20:57:42 +0300

    test: add more tessts

--- test/SocialRecovery.spec.ts
+++ test/SocialRecovery.spec.ts
@@ -12,10 +12,12 @@ describe('SocialRecovery', function () {
   let owner: SignerWithAddress,
     guardianOne: SignerWithAddress,
     guardianTwo: SignerWithAddress,
-    guardianThree: SignerWithAddress
+    guardianThree: SignerWithAddress,
+    random: SignerWithAddress
+
 
   this.beforeAll(async function () {
-    [owner, guardianOne, guardianTwo, guardianThree] = await ethers.getSigners()
+    [owner, guardianOne, guardianTwo, guardianThree, random] = await ethers.getSigners()
 
     const SocialRecovery = await hre.ethers.getContractFactory('SocialRecovery')
     const ERC725Account = await hre.ethers.getContractFactory('LSP0ERC725Account')
@@ -27,19 +29,40 @@ describe('SocialRecovery', function () {
     SocialRecovery.connect(owner)
   })
 
-  
-  // -- setters for strategy: store a signed message
-  // https://docs.ethers.io/v4/cookbook-signing.html
-  it('should add a superGuardian', async function () {
-    await socialRecovery.addSuperGuardian(guardianOne.address)
-    expect(await socialRecovery.getSuperGuardians()).to.have.length(1);
+  describe('when adding a superGuardian', () => {
+    it('should add a superGuardian', async function () {
+      await socialRecovery.addSuperGuardian(guardianOne.address)
+      expect(await socialRecovery.getSuperGuardians()).to.have.length(1);
+    })
+
+    it('should remove an existing superGuardian', async () => {
+      // add
+      await socialRecovery.addSuperGuardian(guardianTwo.address)
+      expect(await socialRecovery.getSuperGuardians()).to.have.length(1)
+      // remove
+      await socialRecovery.removeSuperGuardian(guardianTwo.address)
+      expect(await socialRecovery.getSuperGuardians()).to.have.length(0)
+
+    })
+
   })
-  
-  it('should set a signed message by a superGuardian', async function () {
-    const message = 'SECRET_PHRASE'
-    const signedMessage = await guardianOne.signMessage(message)
-    await socialRecovery.addGuardianSignature(guardianOne.address, signedMessage)
-    expect(await socialRecovery.retrieveSignature(guardianOne.address)).to.be.a.string
+
+  describe('when adding signedMessage messages', () => {
+    it('should set a signed message by a superGuardian', async function () {
+      const message = 'SECRET_PHRASE'
+      const signedMessage = await guardianOne.signMessage(message)
+      await socialRecovery.addGuardianSignature(guardianOne.address, signedMessage)
+      expect(await socialRecovery.retrieveSignature(guardianOne.address)).to.be.a.string
+    })
+
+    it('should revert when non-SuperGuardian tries to add a signature', async () => {
+      const message = 'SECRET_PHRASE'
+      const signedMessage = await guardianThree.signMessage(message)
+ 
+      expect(await socialRecovery.addGuardianSignature(guardianThree.address, signedMessage)).to.be.revertedWith('Caller Must be a superGuardian')
+
+    })
   })
-  
+
+
 })
