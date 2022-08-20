70a99f9a2f69b94057ad900363df4f406cb6b1a2
naftalimurgor <murgornaftali@gmail.com>
2022-08-20 20:54:09 +0300

    fix: add SPDX-License-Identfier: MIT

--- contracts/ISocialRecovery.sol
+++ contracts/ISocialRecovery.sol
@@ -1,7 +1,9 @@
+// SPDX-License-Identifier: MIT
+
 interface ISocialRecovery {
   function addGuardianSignature(address superGuardian, string memory message) external;
   function removeGuardianSignature(address superGuardian) external;
-  function retrieveSignature(address superGuardian) external returns(string memory);
+  function retrieveSignature(address superGuardian, string memory secretPhrase) external returns(string memory);
   function addSuperGuardian(address superGuardian) external;
   function removeSuperGuardian(address superGuardian) external;
   function getSuperGuardians() external view returns (address[] memory);
